6d2dc440d90ccdda34d696b6d9912e8800fee8c1
Dave Steinberg <davidms@ca.ibm.com>
2019-07-31 20:52:06 -0400

    feat(cloudEnv): new cloudEnv abstraction for information about the CF or Kubernetes environment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* eslint global-require: "off" */
 module.exports = {
   appEnv: require("./lib/appEnv"),
+  cloudEnv: require("./lib/cloudEnv"),
   time: require("./lib/time"),
   errors: require("./lib/errors"),
   logger: require("./lib/logger"),
